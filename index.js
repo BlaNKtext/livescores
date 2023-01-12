c6517d5e4b9cd42e89bc2a2fd8b85830bfc47a48
Noah <blanktextt@gmail.com>
2023-01-12 14:20:28 -0500

    Small changes

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 const url = require("url");
-const cors = require("cors");
 var a = [];
-app.use(cors({ origin: true }));
 app.get("/post",(req, res) => {
   var q = url.parse(req.url, true).query;
   if (q.title || q.sid) {
@@ -13,22 +11,14 @@ app.get("/post",(req, res) => {
     a.shift();
   }
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
   res.end("");
 });
 app.get("/get",(req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
   res.end(JSON.stringify(a));
 });
 app.get("/", (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Access-Control-Allow-Origin', '*');
   res.send('Routes are /post and /get');
 });
 app.listen(4444, () => {
-  console.log("Listening on https://127.0.0.1:4444")  
+  console.log("Listening on http://localhost:4444")  
 });
