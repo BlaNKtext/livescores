75a6fa6bb77b257a233233891b17a00bce8b0926
BlaNKtext <blanktextt@gmail.com>
2024-07-28 17:22:19 -0400

    Cors error fix hopefully

--- worker.js
+++ worker.js
@@ -1,30 +1,37 @@
 import { AutoRouter } from 'itty-router'
 const router = AutoRouter()
 import url  from 'url'
-const a = []
+const playString = []
 router.get("/", () => {
-  return new Response("Routes are /get and /post")
+  return new Response("Whoops! Are you sure you know what you're doing?", {
+  method: 'GET',
+  mode: 'cors',
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*"}
+  })
 })
-
 router.get("/get",(req, res) => {
-  const returnData = JSON.stringify(a)
-  return new Response(returnData, {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"}
+    const returnData = JSON.stringify(playString)
+    return new Response(returnData, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"}
+    })
   })
-})
-
-router.get("/post",(req, res) => {
-  let q = url.parse(req.url, true).query
-  if (q.title || q.sid) {a.push(q)}
-  if (a.length > 16) {a.shift()}
-  return new Response("", {headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*"}
+  router.get("/post",(req, res) => {
+    let q = url.parse(req.url, true).query
+    if (q.title || q.sid) {playString.push(q)}
+    if (playString.length > 16) {playString.shift()}
+    return new Response("", {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"}
+    })
   })
-})
-
 router.all("*", () => new Response("This route doesnt exist, silly!", { status: 404 }))
-
 export default router
