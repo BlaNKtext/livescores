adc2905ab1b710c03edb80067730ccddd9983f46
BlaNKtext <blanktextt@gmail.com>
2024-07-30 00:05:38 -0400

    Utilized autocorsify

--- worker.js
+++ worker.js
@@ -1,44 +1,23 @@
-import { AutoRouter } from 'itty-router'
-const router = AutoRouter()
+import { AutoRouter, cors } from 'itty-router'
 import url  from 'url'
+const { preflight, corsify } = cors() 
+const router = AutoRouter({
+  before: [preflight],
+  finally: [corsify]
+})
 const playString = []
 router.get("/", () => {
-  return new Response("Whoops! Are you sure you know what you're doing?", {
-  method: 'GET',
-  mode: 'cors',
-  headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*"}
-  })
+  return new Response('Whoops! Are you sure you know what youre doing?')
 })
 router.get("/get",(req, res) => {
     const returnData = JSON.stringify(playString)
-    return new Response(returnData, {
-      method: 'GET',
-      mode: 'cors',
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"}
-    })
+    return new Response(returnData)
   })
   router.get("/post",(req, res) => {
     let q = url.parse(req.url, true).query
     if (q.title || q.sid) {playString.push(q)}
     if (playString.length > 16) {playString.shift()}
-    return new Response("", {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"}
-    })
+    return new Response("")
   })
-router.all("*", () => new Response("This route doesnt exist, silly!", {
-   status: 404,
-   method: 'POST',
-      mode: 'cors',
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"}
-}))
+router.all("*", () => new Response("This route doesnt exist, silly!", {status: 404}))
 export default router
